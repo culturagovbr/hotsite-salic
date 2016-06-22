24b5a41b113811b96fb8bfe96935bc526cb0590c
Jamile Maeda <jamile.maeda@cultura.gov.br>
2016-06-22 18:45:37 -0300

    Mudanças na página de Atendimento ao Proponente

--- MenuAtendimento.php
+++ MenuAtendimento.php
@@ -18,7 +18,7 @@
             <link rel="stylesheet" type="text/css" href="rs-plugin/css/settings.css" media="screen" />
             <!--[if IE 9]>
             <link rel="stylesheet" type="text/css" href="css/ie9.css" />
-            <![endif]-->   
+            <![endif]-->
 
             <link rel="icon" type="image/png" href="images/fevicon.png">
 
@@ -46,7 +46,7 @@
                 <div class="container">
                     <!--Title / Beadcrumb-->
                     <div class="inner-page-title-wrap col-xs-12 col-md-12 col-sm-12">
-                        <div class="bread-heading"><h1>E-mails</h1></div>
+                        <div class="bread-heading"><h1>Atendimento ao proponente</h1></div>
                         <!--<div class="bread-crumb pull-right">
                         <ul>
                         <li><a href="index.php">Home</a></li>
@@ -60,135 +60,72 @@
             <div class="container iflip">
 
                 <div class="row">
+                    <div>
+                            A Diretoria de Incentivo à Cultura, da Secretaria de Fomento e Incentivo à Cultura do MinC, é responsável, entre outras atividades, pela gestão dos processos submetidos ao mecanismo de Incentivo Fiscal da Lei Rouanet e possui, entre outras, três coordenações-gerais que realizam atividades de rotina relacionadas ao Incentivo Fiscal. São elas:
+                           </br></br>
 
-                    <!--<div class="col-md-12 col-xs-12 col-sm-12 pull-left subtitle ibg-transparent">E-mails</div>-->
+                            <img style="margin-left: 17%" src="images/Atendimento.png"></br></br>
 
-                    <div class="col-md-3 col-lg-3 col-sm-6 col-xs-12">
+                            <h5><strong>Coordenação-Geral de Análise de Projetos de Incentivos Fiscais</strong></h5>
 
-                        <div class="flip-box-wrap">
-                            <div class="flip-box auto horizontal_flip_left" data-animation="" data-animation-delay="03" >
-                                <div class="ifb-flip-box">
+                            É responsável pela admissibilidade, ou seja, a entrada do projeto cultural que pretende efetivar captação de recursos por meio do mecanismo de incentivo fiscal. Orienta sobre como efetuar a inserção da proposta no Sistema de Apoio às Leis de Incentivo à Cultura (Salic). Procede as diligências das Unidades Técnicas, a avaliação da Comissão Nacional de Incentivo à Cultura (CNIC), a aprovação/reprovação e a publicação no Diário Oficial da União. </br></br>
+                            Email: <a style="color:#34a825" href="mailto:proposta.incentivo@cultura.gov.br">proposta.incentivo@cultura.gov.br</a></br>
+                            Telefone: (61) 2024-2060
+                            </br>
+                            </br>
 
-                                    <div class="ifb-face ifb-front" style="background-color: #cccccc">
-                                        <div class="flip-box-icon">
-                                            <div class=" " data-animation="" data-animation-delay="03">
-                                                <img src="images/img_foto.png">
-                                            </div>
-                                        </div>
+                            <h5><strong>Coordenação-Geral de Acompanhamento e Avaliação</strong></h5>
+                            É responsável pelo monitoramento da execução do projeto cultural, envolvendo abertura das contas, movimentações financeiras, ajustes diversos, vistorias in loco e avaliação técnica do cumprimento do objeto.</br></br>
+                            Emails: <a style="color:#34a825" href="mailto:acompanhamento.incentivo@cultura.gov.br">acompanhamento.incentivo@cultura.gov.br</a>; <a style="color:#34a825" href="mailto:avaliacaoobjeto.incentivo@cultura.gov.br">avaliacaoobjeto.incentivo@cultura.gov.br</a>
+                            </br>Telefone: (61) 2024-2040
+                            </br>
+                            </br>
 
-                                        <h3 class="flip-box-head-txt">Titulo do Texto</h3>
-                                        <p>Para iniciar o primeiro projeto com recursos do mecanismo de Incentivo Fiscal da Lei Rouanet, seguem alguns passos iniciais...</p>
-                                    </div><!-- END .front -->
+                            <h5><strong>Coordenação-Geral de Prestação de Contas</strong></h5>
+                            É responsável pela avaliação financeira da prestação de contas, envolvendo a verificação de documentos fiscais que comprovam o uso dos recursos financeiros como notas fiscais, recibos, faturas entre outros, o envio de diligências, a elaboração de relatórios técnico-financeiros e outros procedimentos.</br></br>
+                            Email: <a style="color:#34a825" href="mailto:prestacaodecontas.incentivo@cultura.gov.br">prestacaodecontas.incentivo@cultura.gov.br</a></br>
+                            Telefone: (61) 2024-2090
+                            </br>
+                            </br>
 
-                                    <div class="ifb-face ifb-back flip-backface">
-                                        <h3>Titulo do Texto</h3>
-                                        <p>Para iniciar o primeiro projeto com recursos do mecanismo de Incentivo Fiscal da Lei Rouanet, seguem alguns passos iniciais...</p>
-                                        <div class="flip_link"><a href="PrimeirasOrientacoes.php" class="flip-read-more">Veja mais!</a></div>
-                                    </div><!-- END .back -->
+                            <h5><strong>Coordenação de Análise e Aprovação de Projetos Incentivados (Audiovisual)</strong></h5>
+                            Monitora a admissibilidade, ou seja, a entrada do projeto cultural com o apoio do mecanismo de incentivo fiscal. Orienta sobre como efetuar a inserção da proposta no Sistema Salic, assim como as diligências das Unidades Técnicas, avaliação da Comissão Nacional de Incentivo à Cultura (CNIC), aprovação e publicação no Diário Oficial da União.</br></br>
+                            Telefone: (61) 2024-2848
+                            </br>
+                            </br>
 
-                                </div> <!-- ifb-flip-box -->
-                            </div> <!-- flip-box -->
-                        </div>
-                    </div>
+                            <h5><strong>Coordenação de Acompanhamento à Execução de Projetos Audiovisuais</strong></h5>
+                            Monitora a execução do projeto (mecanismos de incentivo fiscal e FNC), envolvendo abertura das contas, movimentações financeiras, ajustes diversos, avaliação técnica do cumprimento do objeto e vistorias in loco.</br></br>
+                            Telefone: (61) 2024-2934
+                            </br>
+                            </br>
 
+                            <h5><strong>Coordenação de Prestação de Contas de Projetos Incentivados (Audiovisual)</strong></h5>
+                            Analisa a fase de prestação de contas, instruindo sobre preenchimento de formulários, resposta a diligências, elaboração de relatórios técnicos financeiros e outros procedimentos.</br></br>
+                            Telefone: (61) 2024-2837
+                            </br>
+                            </br>
 
-                    <div class="col-md-3 col-lg-3 col-sm-6 col-xs-12">
-
-                        <div class="flip-box-wrap">
-                            <div class="flip-box auto horizontal_flip_left" data-animation="" data-animation-delay="03" >
-                                <div class="ifb-flip-box">
-
-                                    <div class="ifb-face ifb-front" style="background-color: #cccccc">
-                                        <div class="flip-box-icon">
-                                            <div class=" " data-animation="" data-animation-delay="03">
-                                                <img src="images/img_foto.png">
-                                            </div>
-                                        </div>
-
-                                        <h3 class="flip-box-head-txt">Titulo do Texto</h3>
-                                        <p>Para iniciar o primeiro projeto com recursos do mecanismo de Incentivo Fiscal da Lei Rouanet, seguem alguns passos iniciais...</p>
-                                    </div><!-- END .front -->
-
-                                    <div class="ifb-face ifb-back flip-backface">
-                                        <h3>Titulo do Texto</h3>
-                                        <p>Para iniciar o primeiro projeto com recursos do mecanismo de Incentivo Fiscal da Lei Rouanet, seguem alguns passos iniciais...</p>
-                                        <div class="flip_link"><a href="PrimeirasOrientacoes.php" class="flip-read-more">Veja mais!</a></div>
-                                    </div><!-- END .back -->
-
-                                </div> <!-- ifb-flip-box -->
-                            </div> <!-- flip-box -->
-                        </div>
-                    </div>
-
-
-
-
-                    <div class="col-md-3 col-lg-3 col-sm-6 col-xs-12">
-
-                        <div class="flip-box-wrap">
-                            <div class="flip-box auto horizontal_flip_left" data-animation="" data-animation-delay="03" >
-                                <div class="ifb-flip-box">
-
-                                    <div class="ifb-face ifb-front" style="background-color: #cccccc">
-                                        <div class="flip-box-icon">
-                                            <div class=" " data-animation="" data-animation-delay="03">
-                                                <img src="images/img_foto.png">
-                                            </div>
-                                        </div>
-
-                                        <h3 class="flip-box-head-txt">Titulo do Texto</h3>
-                                        <p>Para iniciar o primeiro projeto com recursos do mecanismo de Incentivo Fiscal da Lei Rouanet, seguem alguns passos iniciais...</p>
-                                    </div><!-- END .front -->
-
-                                    <div class="ifb-face ifb-back flip-backface">
-                                        <h3>Titulo do Texto</h3>
-                                        <p>Para iniciar o primeiro projeto com recursos do mecanismo de Incentivo Fiscal da Lei Rouanet, seguem alguns passos iniciais...</p>
-                                        <div class="flip_link"><a href="PrimeirasOrientacoes.php" class="flip-read-more">Veja mais!</a></div>
-                                    </div><!-- END .back -->
-
-                                </div> <!-- ifb-flip-box -->
-                            </div> <!-- flip-box -->
-                        </div>
-                    </div>
-
-
-
-
-                    <div class="col-md-3 col-lg-3 col-sm-6 col-xs-12">
-
-                        <div class="flip-box-wrap">
-                            <div class="flip-box auto horizontal_flip_left" data-animation="" data-animation-delay="03" >
-                                <div class="ifb-flip-box">
-
-                                    <div class="ifb-face ifb-front" style="background-color: #cccccc">
-                                        <div class="flip-box-icon">
-                                            <div class=" " data-animation="" data-animation-delay="03">
-                                                <img src="images/img_foto.png">
-                                            </div>
-                                        </div>
-
-                                        <h3 class="flip-box-head-txt">Titulo do Texto</h3>
-                                        <p>Para iniciar o primeiro projeto com recursos do mecanismo de Incentivo Fiscal da Lei Rouanet, seguem alguns passos iniciais...</p>
-                                    </div><!-- END .front -->
-
-                                    <div class="ifb-face ifb-back flip-backface">
-                                        <h3>Titulo do Texto</h3>
-                                        <p>Para iniciar o primeiro projeto com recursos do mecanismo de Incentivo Fiscal da Lei Rouanet, seguem alguns passos iniciais...</p>
-                                        <div class="flip_link"><a href="PrimeirasOrientacoes.php" class="flip-read-more">Veja mais!</a></div>
-                                    </div><!-- END .back -->
-
-                                </div> <!-- ifb-flip-box -->
-                            </div> <!-- flip-box -->
-                        </div>
-                    </div>
-
-                </div>
-                
-                <div class="row">
+                            <h5><strong>Núcleo de Atendimento Presencial da Secretaria de Fomento e Incentivo à Cultura</strong></h5>
+                            Atendimento presencial a proponentes.</br></br>
+                            Endereço: Esplanada dos Ministérios, Bloco B, Térreo, CEP: 70.068-900 Brasília-DF</br>
+                            Telefone: (61) 2024-2098 / 2024-2249</br>
+                            Atendimento: 9h às 12h e 14h às 17h</br>
+                            </br>
+                            </br>
+                                  Para acessar nossa Ouvidoria, <a style="color:#34a825" href="http://ouvidoria.cultura.gov.br/ouvidoria/login.jsp">clique aqui</a>.</br>
+                                  Para acessar o Sistema eletrônico de serviços de informação ao cidadão, <a style="color:#34a825" href="http://www.acessoainformacao.gov.br/sistema/site/index.html?ReturnUrl=%2fsistema%2f">clique aqui</a>.
+                                </br></br>
+                            Para mais contatos, acesse: <a style="color:#34a825" href="http://www.cultura.gov.br/fale-com-o-minc">http://www.cultura.gov.br/fale-com-o-minc</a>
+                            </br>
+                            </br>
+                            </br>
+                            </br>
+               </div>
 
                         <!--Contact Sidebar-->
                         <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-
+                          <!--
                             <div class="side-bar-contact">
                                 <div class="form-title-text wow fadeInRight" data-wow-delay="0.5s" data-wow-offset="100">Informações</div>
                                 <ul class="contact-page-list wow fadeInRight" data-wow-delay="0.5s" data-wow-offset="100">
@@ -212,6 +149,7 @@
                                         <span class="contact-side-txt">Email: <a href="mailto:proposta.incentivo@cultura.gov.br">proposta.incentivo@cultura.gov.br</a></span>
                                     </li>
                                 </ul>
+                              -->
 
                                 <div class="form-title-text wow fadeInRight" data-wow-delay="0.5s" data-wow-offset="120">Redes Sociais</div>
 
@@ -232,13 +170,8 @@
                                 </ul>
                             </div>
 
-                        </div><!--Contact Sidebar end-->
-
-
-                    </div><!--Contact Form end-->
-
+                    </div>
                 </div>
-
             </div>
 
             <!--Footer Start-->
@@ -248,7 +181,7 @@
         <section>
             <div id="settings">
                 <div class="colors">
-                    <div class="panel-title">Style Switcher</div> 
+                    <div class="panel-title">Style Switcher</div>
                     <div class="panel-color-title">Color Schemes</div>
                     <ul>
                         <li><a title="maroon" class="color1 color-switch"><i class="fa fa-check"></i></a></li>
@@ -278,7 +211,7 @@
 
         <!--JS Inclution-->
         <script type="text/javascript" src="js/jquery.min.js"></script>
-        <script type="text/javascript" src="js/jquery-ui-1.10.3.custom.min.js"></script>  
+        <script type="text/javascript" src="js/jquery-ui-1.10.3.custom.min.js"></script>
         <script type="text/javascript" src="bootstrap-new/js/bootstrap.min.js"></script>
         <script type="text/javascript" src="rs-plugin/js/jquery.themepunch.tools.min.js"></script>
         <script type="text/javascript" src="rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
